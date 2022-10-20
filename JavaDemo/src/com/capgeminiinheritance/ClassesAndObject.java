974702cb87c508ab0d55be2c38abba62f3a68069
dnyan <dnyan@DESKTOP-TN0PF10>
2022-10-20 15:20:19 +0530

    second commit

---
+++ JavaDemo/src/com/capgeminiinheritance/ClassesAndObject.java
@@ -0,0 +1,12 @@
+package com.capgeminiinheritance;
+
+public class ClassesAndObject {
+	public static void main(String[] args) {
+		Demo car = new Demo();
+		car.setDoors ("closed");
+		car.setSpeed (10);
+		System.out.println(car.run());
+
+	}
+	
+}
