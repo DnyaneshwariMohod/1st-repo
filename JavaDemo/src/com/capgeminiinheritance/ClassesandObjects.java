70bc15024e218c16ec618e26dc9c5c8b55f35e9c
dnyan <dnyan@DESKTOP-TN0PF10>
2022-10-20 14:51:14 +0530

    first commit

---
+++ JavaDemo/src/com/capgeminiinheritance/ClassesandObjects.java
@@ -0,0 +1,6 @@
+//
+package com.capgeminiinheritance;
+
+public class ClassesandObjects {
+
+}
---
+++ JavaDemo/src/com/capgeminiinheritance/JavaClassesAndObjects.java
@@ -0,0 +1,30 @@
+package com.capgeminiinheritance;
+class Car 
+{
+//variable for car
+		private String Doors;
+		private String Engine;
+		private String Driver;
+		private int speed;
+		public void setSpeed(int speed)
+		{
+			this.speed=speed;
+		}
+		public int getSpeed()
+		{
+			return speed;
+		}
+
+}
+public class JavaClassesAndObjects {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car = new Car ();
+		car.setSpeed(15);
+		System.out.println(car.getSpeed());
+		
+	}
+}
+
+
